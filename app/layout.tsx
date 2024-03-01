bdcccf90f4ed1e890c1dc6fda15bf84d92e6202a
Mikias Worash <mikiasworash@gmail.com>
2024-03-01 18:32:25 +0300

    feat: shadcn/ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="container mx-auto">
+          <div className="flex items-start justify-center min-h-screen">
+            <div className="mt-20"> {children}</div>
+          </div>
+        </main>
+      </body>
     </html>
   );
 }
